181ea394794e870ab25c3990661d6eff42966982
AliBaydur <59739580+AliBaydur@github.com>
2024-06-08 23:19:53 +0300

    * AutoMapper. * Record type.. * Paket Kurulumu. * Mapping Profile. * Servis Kaydı. * ServiceManager Düzenlemesi. * Sunum katmanı Düzenlemesi.

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Exceptions;
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Exceptions;
 using Entities.Models;
 using Repositories.Contracts;
 using Services.Contracts;
@@ -14,11 +16,13 @@ namespace Services
     {
         private readonly IRepositoryManager _manager;
         private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
 
-        public BookManager(IRepositoryManager manager, ILoggerService logger)
+        public BookManager(IRepositoryManager manager, ILoggerService logger, IMapper mapper)
         {
             _manager = manager;
             _logger = logger;
+            _mapper = mapper;
         }
 
         public Book CreateOneBook(Book book)
@@ -54,15 +58,15 @@ namespace Services
             return book;
         }
 
-        public void UpdateOneBook(int id, Book book, bool trackChanges)
+        public void UpdateOneBook(int id, BookDtoForUpdate bookDto, bool trackChanges)
         {
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-                throw new BookNotFoundException(id);           
-           
-            entity.Title = book.Title;
-            entity.Price = book.Price;  
+                throw new BookNotFoundException(id);
+
+            //Mapping             
+            entity = _mapper.Map<Book>(bookDto);
 
             _manager.Book.Update(entity);
             _manager.Save();
