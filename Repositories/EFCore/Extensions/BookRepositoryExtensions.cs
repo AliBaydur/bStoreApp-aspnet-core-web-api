2d139ce4b8320c8917e17201ae759f44197cb5c5
AliBaydur <59739580+AliBaydur@github.com>
2024-07-22 20:38:02 +0300

    Sorting Sorting Implementation Query Strings System.Linq.Dynamic.Core Order Query Builder Repository Extensions

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Repositories.EFCore.Extensions
 {
@@ -19,8 +21,21 @@ namespace Repositories.EFCore.Extensions
             if (string.IsNullOrWhiteSpace(searchTerm))
                 return books;
 
-            var  lowerCaseTerm = searchTerm.Trim().ToLower();
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
             return books.Where(b => b.Title.ToLower().Contains(searchTerm));
+        }
+
+        public static IQueryable<Book> Sort(this IQueryable<Book> books, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return books.OrderBy(b => b.Id);
+                       
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Book>(orderByQueryString);
+
+            if (orderQuery is null)
+                return books.OrderBy(b => b.Id);
+
+            return books.OrderBy(orderQuery);           
         }
     }
 }
