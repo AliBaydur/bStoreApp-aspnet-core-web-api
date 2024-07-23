71eef17db33cc0760b61b15cad17183696c23457
AliBaydur <59739580+AliBaydur@github.com>
2024-07-23 22:11:41 +0300

    Data Shaping Dynamic Typing Reflection PropertyInfo Data Shaper Implementation Service Register

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -7,6 +7,7 @@ using Repositories.Contracts;
 using Services.Contracts;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,12 +19,14 @@ namespace Services
         private readonly IRepositoryManager _manager;
         private readonly ILoggerService _logger;
         private readonly IMapper _mapper;
+        private readonly IDataShaper<BookDto> _shaper;
 
-        public BookManager(IRepositoryManager manager, ILoggerService logger, IMapper mapper)
+        public BookManager(IRepositoryManager manager, ILoggerService logger, IMapper mapper, IDataShaper<BookDto> shaper)
         {
             _manager = manager;
             _logger = logger;
             _mapper = mapper;
+            _shaper = shaper;
         }
 
         public async Task<BookDto> CreateOneBookAsync(BookDtoForInsertion bookDto)
@@ -41,15 +44,16 @@ namespace Services
             await _manager.SaveAsync();
         }
 
-        public async Task<(IEnumerable<BookDto> books, MetaData metaData)> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
+        public async Task<(IEnumerable<ExpandoObject> books, MetaData metaData)> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
             if (!bookParameters.ValidPriceRange)
                 throw new PriceOutofRangeBadRequestException();
 
-            var booksWithMetaData = await _manager.Book.GetAllBooksAsync(bookParameters, trackChanges);
+            var booksWithMetaData = await _manager.Book.GetAllBooksAsync(bookParameters, trackChanges);            
             var booksDto = _mapper.Map<IEnumerable<BookDto>>(booksWithMetaData);
+            var shapedData = _shaper.ShapeData(booksDto, bookParameters.Fields);
 
-            return (booksDto,booksWithMetaData.MetaData);
+            return (books:shapedData,metaData : booksWithMetaData.MetaData);
         }
 
         public async  Task<(BookDtoForUpdate bookDtoForUpdate, Book book)> GetOneBookForPatchAsync(int id, bool trackChanges)
