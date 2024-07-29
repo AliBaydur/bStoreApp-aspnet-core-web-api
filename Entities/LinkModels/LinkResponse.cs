705dc4808320789358b62e7f080586a3bfc1adca
AliBaydur <59739580+AliBaydur@github.com>
2024-07-29 23:22:18 +0300

    Hypermedia as the Engine of Application state. HATEOAS Link, LinkResourceBase, LinkCollectionWrap Entity, Shaped Entity

---
+++ Entities/LinkModels/LinkResponse.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+
+
+namespace Entities.LinkModels
+{
+    public class LinkResponse
+    {
+        public bool HasLinks { get; set; }
+        public List<Entity> ShapedEntities { get; set; }
+        public LinkCollectionWrapper<Entity> LinkedEntites { get; set; }
+
+        public LinkResponse()
+        {
+            ShapedEntities = new List<Entity>();
+            LinkedEntites = new LinkCollectionWrapper<Entity>();
+
+        }
+    }
+}
